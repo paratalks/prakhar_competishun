7dd0cbf812dec0f8f5fe556cb302676248601ac3
yash24shukla <thisform240@gmail.com>
2024-11-05 17:28:47 +0530

    website with responsiveness - 59

--- app/page.tsx
+++ app/page.tsx
@@ -151,7 +151,7 @@ export default function Home() {
                   delay: 0.2,
                 }}
                 className={
-                  "absolute select-none sm:w-14 w-10 top-1/2 left-[20%] -z-40"
+                  "absolute hidden sm:flex select-none sm:w-14 w-10 top-1/2 left-[20%] -z-40"
                 }
                 src={icons.rolledDegree}
                 alt={"Rolled Degree Icom"}
